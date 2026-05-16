333539af7a8cf3149d84c427411b1e9d9ed9afd6
dhruvilshah <dhruvilshah23u@gmail.com>
2026-05-16 10:53:26 +0530

    sync now

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -2,6 +2,20 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 
+const clerkAppearance = {
+  __internal: {
+    isIntercom: false,
+  },
+};
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider
+      appearance={clerkAppearance}
+      signInUrl="/signin"
+      signUpUrl="/register"
+    >
+      {children}
+    </ClerkProvider>
+  );
 }
